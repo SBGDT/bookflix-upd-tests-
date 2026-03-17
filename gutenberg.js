6f72aafaf5761beb1cf1ef49f70204af38bd9aaf
SBGDT <lialroab2@gmail.com>
2026-03-16 20:25:02 -0400

    Create gutenberg.js

---
+++ gutenberg.js
@@ -0,0 +1,15 @@
+const gutenbergBooks = {
+
+"moby dick":"https://www.gutenberg.org/files/2701/2701-h/2701-h.htm",
+
+"frankenstein":"https://www.gutenberg.org/files/84/84-h/84-h.htm",
+
+"alice":"https://www.gutenberg.org/files/11/11-h/11-h.htm",
+
+"dracula":"https://www.gutenberg.org/files/345/345-h/345-h.htm",
+
+"sherlock holmes":"https://www.gutenberg.org/files/1661/1661-h/1661-h.htm",
+
+"pride and prejudice":"https://www.gutenberg.org/files/1342/1342-h/1342-h.htm"
+
+};
